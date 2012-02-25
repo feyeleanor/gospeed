8091a82aaa3d71dc7cbbe6ea0b464e58ebcda6de
Eleanor McHugh <eleanor@games-with-brains.com>
2012-02-25 11:56:54 +0000

    Additional benchmarks for map creation

--- baseline_benchmark_test.go
+++ baseline_benchmark_test.go
@@ -338,12 +338,52 @@ func BenchmarkBaselineNewSlice(b *testing.B) {
 	for i := 0; i < b.N; i++ { _ = make([]int, 2) }
 }
 
-func BenchmarkBaselineNewMapLiteral(b *testing.B) {
+func BenchmarkBaselineNewMapLiteralIntToInt(b *testing.B) {
 	for i := 0; i < b.N; i++ { _ = map [int]int{} }
 }
 
-func BenchmarkBaselineNewMap(b *testing.B) {
+func BenchmarkBaselineNewMapLiteralIntToInterface(b *testing.B) {
+	for i := 0; i < b.N; i++ { _ = map [int]interface{}{} }
+}
+
+func BenchmarkBaselineNewMapLiteralStringToInt(b *testing.B) {
+	for i := 0; i < b.N; i++ { _ = map [string]int{} }
+}
+
+func BenchmarkBaselineNewMapLiteralStringToInterface(b *testing.B) {
+	for i := 0; i < b.N; i++ { _ = map [string]interface{}{} }
+}
+
+func BenchmarkBaselineNewMapLiteralIntToInt2Item(b *testing.B) {
+	for i := 0; i < b.N; i++ { _ = map [int]int{i: 0, i: 1} }
+}
+
+func BenchmarkBaselineNewMapLiteralIntToInterface2Item(b *testing.B) {
+	for i := 0; i < b.N; i++ { _ = map [int]interface{}{i: 0, i: 1} }
+}
+
+func BenchmarkBaselineNewMapLiteralStringToInt2Item(b *testing.B) {
+	for i := 0; i < b.N; i++ { _ = map [string]int{"0": i, "1": i} }
+}
+
+func BenchmarkBaselineNewMapLiteralStringToInterface2Item(b *testing.B) {
+	for i := 0; i < b.N; i++ { _ = map [string]interface{}{"0": i, "1": i} }
+}
+
+func BenchmarkBaselineNewMapIntToInt(b *testing.B) {
 	for i := 0; i < b.N; i++ { _ = make(map [int]int) }
+}
+
+func BenchmarkBaselineNewMapIntToInterface(b *testing.B) {
+	for i := 0; i < b.N; i++ { _ = make(map [int]interface{}) }
+}
+
+func BenchmarkBaselineNewMapStringToInt(b *testing.B) {
+	for i := 0; i < b.N; i++ { _ = make(map [string]int) }
+}
+
+func BenchmarkBaselineNewMapStringToInterface(b *testing.B) {
+	for i := 0; i < b.N; i++ { _ = make(map [string]interface{}) }
 }
 
 func BenchmarkBaselineSliceCopy(b *testing.B) {
